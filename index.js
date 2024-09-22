1994eb9b384aba76b918e95bd11a64a21c2564c8
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-09-22 15:10:47 +0200

    add fixes to JS file and link the dynamic inputs

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ document.addEventListener('DOMContentLoaded', function () {
   
       // 🚨 Get input values
       const studentNameInput = document.getElementById('studentName');
-      const personalMessageInput = ;
-      const courseNameInput = ; 
+      const personalMessageInput = document.getElementById('personalMessage');
+      const courseNameInput = document.getElementById('courseName');
   
       const studentName = studentNameInput.value;
       const personalMessage = personalMessageInput.value;
@@ -25,8 +25,10 @@ document.addEventListener('DOMContentLoaded', function () {
       }
   
       // 🚨 Generate certificate content dynamically
-      certificateContent. = `
+      certificateContent.innerHTML = `
       <h3>${studentName}</h3>
+      <p>${personalMessage}</p>
+      <h2>${courseName}</h2>
     `;
     
       //  Display the modal
@@ -39,7 +41,7 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   
     //  🚨 Close the modal when the close button is clicked
-    closeModal.('', function () {
+    closeModal.addEventListener('click', function () {
       
     });
   });
